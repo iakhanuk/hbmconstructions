ecc2c1654ecad0a640591f2dbfaa70f8e12b98ec
Husain Shahid Rao <hsr3012kalimg@gmail.com>
2024-05-09 17:21:00 +0530

    add meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,24 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Hydrogen Building & Maintenance",
   description: "Hydrogen Building & Maintenance Ltd is a construction company based in London, UK, specializing in building, renovation, and remodeling services.",
+  openGraph: {
+    type: "website",
+    locale: "en_GB",
+    url: "https://hbmconstruction.co.uk",
+    siteName: "Hydrogen Building & Maintenance",
+    countryName: "United Kingdom",
+    images: [
+      {
+        url: "/images/hbm-cover.png",
+        width: 800,
+        height: 600,
+        alt: "Hydrogen Building & Maintenance",
+      },
+    ],
+    emails: ["contact@hbmconstruction.co.uk", "hydrogenbnm@gmail.com"],
+    phoneNumbers: ["07915368262"],
+  },
+
 };
 
 export default function RootLayout({
